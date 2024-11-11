ad52a23fa504c49021babf5b7492ce7306e32aab
Georgyi <112905032+georgij-tsabiev@users.noreply.github.com>
2024-11-11 16:17:51 +0300

    Поправляет путь у иконки метки на карте

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ const TOKYO_COORDINATES = {
   lng: 139.75232
 };
 const mainPinIcon = L.icon({
-  iconUrl: '/img/main-pin.svg',
+  iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
